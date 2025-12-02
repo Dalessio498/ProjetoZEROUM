2bad824bd106771638c6128d9181ea35d38717d0
Marco Antonio Dalessio <amtoniomarco91@gmail.com>
2025-12-01 21:25:19 -0300

    Delete sla directory

--- sla/teste/teste/src/main/java/teste/teste/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package teste.teste.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import teste.teste.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-}
